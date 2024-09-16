d5535b213ea356f702634f47c626abbc1618f60e
Abigail-DePriest <abby.depriest@gmail.com>
2024-09-16 17:08:20 -0500

    got SignUp Card rendering

--- components/signups/SignUpCard.js
+++ components/signups/SignUpCard.js
@@ -1,30 +1,39 @@
-// components/events/SignUpCard.js
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 
 const SignUpCard = ({ event, onLeaveEvent }) => (
-  <Card className="text-center">
-    <Card.Header>{event.eventTypeName}</Card.Header>
+  <Card className="card" style={{ width: '18rem', margin: '10px', border: '1px solid' }}>
+    <Card.Header style={{ textAlign: 'center', paddingTop: '10px' }}>
+      {event.eventType.eventTypeName}
+    </Card.Header>
     <Card.Body>
-      <Card.Title>Date: {event.eventDate}</Card.Title>
-      <Card.Text>Time: {event.eventTime}</Card.Text>
-      <Card.Text>Location: {event.location}</Card.Text>
-      <Button variant="danger" onClick={() => onLeaveEvent(event.id)}>Leave Event</Button>
+      <Card.Title style={{ textAlign: 'center' }}>
+        Date: {event.eventDate}
+      </Card.Title>
+      <Card.Subtitle className="mb-2 text-muted" style={{ textAlign: 'center' }}>
+        Time: {event.eventTime}
+      </Card.Subtitle>
+      <Card.Text style={{ textAlign: 'center' }}>
+        Location: {event.location}
+      </Card.Text>
+      <Button variant="danger" onClick={() => onLeaveEvent(event.id)} className="m-2">
+        Leave Event
+      </Button>
     </Card.Body>
-    <Card.Footer className="text-muted">Organized by: {event.organizer.name}</Card.Footer>
   </Card>
 );
 
 SignUpCard.propTypes = {
   event: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    eventTypeName: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    eventType: PropTypes.shape({
+      id: PropTypes.number,
+      eventTypeName: PropTypes.string,
+    }),
     eventDate: PropTypes.string.isRequired,
     eventTime: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
-    organizer: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-    }).isRequired,
   }).isRequired,
   onLeaveEvent: PropTypes.func.isRequired,
 };
